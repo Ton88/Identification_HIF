20fd0266f2b1995e583458e25cdeb243ea2cfb53
Wellington Barbosa <42788277+Ton88@users.noreply.github.com>
2024-11-17 18:05:48 -0300

    13 Bus Simulation
    This repository contains the necessary files and instructions to replicate the High Impedance Fault (HIF) identification experiment using MATLAB Simulink, Wavelet transformations, and machine learning algorithms.

---
+++ 13_Bus/Code/B13buss17.m
@@ -0,0 +1,27 @@
+
+% salva em uma váriavel os dados da linha C
+%Ic = Iabc(:,3);
+data = Ic';
+
+% Defina a matriz original com 200.000 linhas
+% Suponha que você tenha uma matriz chamada 'matriz_original' com 300.000 linhas e qualquer número de colunas
+dados = data(1:200000,:);
+% Número de linhas em cada parte
+linhas_por_parte = 200000 / 200; % segundos
+
+% Divida a matriz original em partes
+partes = mat2cell(dados, repmat(linhas_por_parte, 1, 200), size(dados, 2));% repmat(linhas_por_parte, 1, 100)
+
+% Crie uma nova matriz pivotando cada parte
+nova_matriz = cellfun(@transpose, partes, 'UniformOutput', false);
+
+% Converta a nova matriz de células para uma matriz normal
+nova_matriz = cat(1, nova_matriz{:});
+
+
+% Especificar o nome do arquivo
+nome_arquivo = 'Teste_17_11_20s_34Bus_200_linhas_10mile_seg.csv';
+
+
+% Salvar a matriz no arquivo de texto separado por ponto e vírgula
+dlmwrite(nome_arquivo, nova_matriz, ';');
